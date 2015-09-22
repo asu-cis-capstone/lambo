4d561c9647845a8db1d592e1557ae0f07c204079
Skyler <srhewitt@asu.edu>
2015-09-22 13:26:51 -0700

    Add files to test stack. Add gitignore

---
+++ www/index.php
@@ -0,0 +1,25 @@
+Hello World 
+<br />
+<?php
+include_once('config.php');
+
+$connection = new mysqli($server, $user, $password, $db);
+
+if ($connection->connect_error)
+{
+	die("Connection failed: " . $connection->connect_error);
+}
+echo "connected successfully";
+echo "<br />";
+
+$sql = "select * from Test";
+$result = $connection->query($sql);
+echo "Database returned " . $result->num_rows . " row:";
+echo "<br />";
+echo "<table border=solid>";
+while($row = $result->fetch_assoc())
+{
+	echo "<tr> <td>" . $row['Hello'] . "</td><td> " . $row['World'] . "</td></tr>";
+}
+echo "</table>"
+?>
