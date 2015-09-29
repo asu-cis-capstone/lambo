720eac49a5f945d95da10c88ab9b8aa8a4bab66d
Skyler <srhewitt@asu.edu>
2015-09-29 13:29:49 -0700

    Updated instructions in readme, added template file, fixed css link

---
+++ HelloWorld.sql
@@ -0,0 +1,3 @@
+Create Database OnPointDetailing;
+CREATE TABLE `OnPointDetailing`.`Test` ( `Hello` VARCHAR(10) NOT NULL , `World` VARCHAR(10) NOT NULL ) ENGINE = InnoDB;
+INSERT INTO `OnPointDetailing`.`Test` (`Hello`, `World`) VALUES ('Hi', 'Planet')
