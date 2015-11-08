e25ea0f32fef1afa574be75bf420fa3324c2117a
SkylerRH <srhewitt@asu.edu>
2015-11-08 12:15:49 -0700

    Add calendar logic to scheduler.

--- www/scheduler.php
+++ www/scheduler.php
@@ -21,6 +21,96 @@
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="js/ie-emulation-modes-warning.js"></script>
 
+    <!-- Script for interacting with google calendar api-->
+
+    <script type="text/javascript">
+      // Your Client ID can be retrieved from your project in the Google
+      // Developer Console, https://console.developers.google.com
+
+      var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+      var APIKey = 'AIzaSyCrlsIrsTN2-Weeb4oozrzBiAmUT2dFQ3s';
+      var calendarID = 'onpointdetailingtest@gmail.com';
+
+      /**
+       * Load Google Calendar client library. List upcoming events
+       * once client library is loaded.
+       */
+      function loadCalendarApi() {
+        gapi.client.setApiKey(APIKey)
+        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+      }
+
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      function listUpcomingEvents() {
+        var availableTimes = ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00"];
+        var allTimes = ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00"];
+        var startIndex;
+        var endIndex;
+        var startDate = new Date(document.getElementById('date').value.substring(0,4),document.getElementById('date').value.substring(5,7)-1,document.getElementById('date').value.substring(8,10),0,0,0,0);
+        var endDate = new Date (startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 1);
+        var request = gapi.client.calendar.events.list({
+          'calendarId': calendarID,
+          'timeMin': startDate.toISOString(),
+          'timeMax': endDate.toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'orderBy': 'startTime'
+        });
+
+        request.execute(function(resp) {
+
+          var events = resp.items;
+
+          if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+              var event = events[i];
+              startTime = event.start.dateTime.substring(11,16);
+              endTime = event.end.dateTime.substring(11,16);
+
+              for (j = 0; j < availableTimes.length; j++)
+              {
+                if (startTime === availableTimes[j])
+                {
+                  startIndex = j;
+                }
+                if (endTime === availableTimes[j])
+                {
+                  endIndex = j;
+                }
+                if (j>=startIndex && j===endIndex)
+                  availableTimes.splice(startIndex,endIndex-startIndex);
+              }
+            }
+          }
+
+          var times = "";
+          for (k = 0; k < allTimes.length; k++)
+          {
+            if ($.inArray(allTimes[k],availableTimes) != -1)
+              times += '<option value="'+allTimes[k]+'">'+allTimes[k]+'</option>';
+            else
+              times += '<option disabled="true" value="'+allTimes[k]+'">'+allTimes[k]+'</option>';
+          }
+          listTimes(times);
+        });
+      }
+
+      function listTimes(content){
+        document.getElementById('time').innerHTML = content;
+      }
+
+      function getServiceName(){
+        document.getElementById('service').innerHTML = window.location.hash.substring(1);
+      }
+
+    </script>
+    <script src="https://apis.google.com/js/client.js">
+    </script>
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -57,7 +147,7 @@
     <div class="container">
 
       <h2>Appointment Scheduling</h2>
-      <p>You are scheduling an appointment for %SERVICE%</p>
+      <p>You are scheduling an appointment for <div id='service'><?php echo $_GET["service"]; ?></div></p>
       <form role="form">
         <div class="form-group">
           <label for="First Name">First Name:</label>
@@ -71,10 +161,10 @@
           <label for="Model">Vehicle Make and Model:</label>
           <input type="text" class="form-control" id="model" placeholder="Make & Model">
           <label for="Date">Which date would you like?</label>
-          <input type="date" class="form-control" id="date">
+          <input type="date" class="form-control" id="date" onchange="loadCalendarApi();">
           <label for="time">Please pick an available start time. Your service will take approximately %NUMBER% hours</label>
           <select class="form-control" id="time">
-            <option value="1030">10:30</option>
+            <option value="null">Select a date to see times</option>
           </select>
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
